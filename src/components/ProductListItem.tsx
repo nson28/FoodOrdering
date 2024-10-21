73b5b586d2291e77b39bff69f6e42d2ba3206317
nson28 <nelsontejara@vsu.edu.ph>
2024-10-21 14:56:48 +0800

    added product details screen

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, Text, View , Image, Pressable} from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 
 export const defaultPizzaImage =
@@ -14,16 +15,17 @@ type ProductListItemProps = {
 
 const ProductListItem = ({product}: ProductListItemProps)=>{
   return(
-    <View style={styles.container}>
+    <Link href= {`/menu/${product.id}`} asChild >
+    <Pressable style={styles.container}>
       <Image source= {{uri: product.image || defaultPizzaImage}} 
              style ={styles.image} 
              resizeMode='contain'
              
         />
       <Text style={ styles.title }> {product.name}</Text>
-      <Text style= { styles.price }>{product.price}</Text>      
-    </View>
-
+      <Text style= { styles.price }>{product.price}</Text>            
+    </Pressable>
+    </Link> 
   )
 }
 
