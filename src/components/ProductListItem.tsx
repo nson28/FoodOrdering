d4c77b6ccd2871c4294a6b3ff2cd2e6e6c448e55
nson28 <nelsontejara@vsu.edu.ph>
2024-10-21 12:01:06 +0800

    using flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -15,7 +15,11 @@ type ProductListItemProps = {
 const ProductListItem = ({product}: ProductListItemProps)=>{
   return(
     <View style={styles.container}>
-      <Image source= {{uri: product.image || defaultPizzaImage}} style ={styles.image} />
+      <Image source= {{uri: product.image || defaultPizzaImage}} 
+             style ={styles.image} 
+             resizeMode='contain'
+             
+        />
       <Text style={ styles.title }> {product.name}</Text>
       <Text style= { styles.price }>{product.price}</Text>      
     </View>
@@ -29,7 +33,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     padding:10,
-    borderRadius:20,    
+    borderRadius:20,
+    flex:1, 
+    maxWidth:'50%',     
   },
   image: {
     width: '100%',
