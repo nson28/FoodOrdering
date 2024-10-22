b3668c38bce7b939fcdfe4685a5038bb8a82546e
nson28 <nelsontejara@vsu.edu.ph>
2024-10-22 08:53:35 +0800

    change the layout file

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from 'expo-router';
 
 export default function MenuStack (){
-    return <Stack />
+    return <Stack> 
+        <Stack.Screen name="index" options={{title:'Menu'}}/>
+    </Stack>
 
 }
