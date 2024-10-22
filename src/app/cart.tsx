fb2f81a162a8b01e878b8891bb06ba44e72e9911
nson28 <nelsontejara@vsu.edu.ph>
2024-10-22 13:25:51 +0800

    added modal

---
+++ src/app/cart.tsx
@@ -0,0 +1,17 @@
+import { View, Text,  Platform} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+
+const CartScreen = () =>{
+    return(
+        <View>
+            <Text>cart1</Text>
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+        </View>
+
+    );
+
+};
+
+
+export default CartScreen;
