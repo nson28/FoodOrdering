32874bf81574109b7ae8aaabcbc9b8d7ed2770e3
nson28 <nelsontejara@vsu.edu.ph>
2024-12-05 10:55:20 +0800

    added image for creating a product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,14 @@
 import Button from '@/src/components/Button';
+import { defaultPizzaImage } from '@/src/components/ProductListItem';
 import { useState } from 'react';
-import {View, Text, StyleSheet, TextInput } from 'react-native';
+import {View, Text, StyleSheet, TextInput, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
 
 const CreateProductScreen =() => {
 
     const [name,setName] = useState('');
     const [price,setPrice] = useState('');
+    const [errors, setErrors] = useState('');
 
     const resetFields = () => {
         setName('');
@@ -13,13 +16,40 @@ const CreateProductScreen =() => {
     }
 
 
+    const validateInput = () => {
+        setErrors('');        
+        if(!name) {
+            setErrors('Name is required');
+            return false;
+        }
+        if(!price){
+            setErrors('Price is required')
+            return false;
+        }
+        if (isNaN(parseFloat(price))){
+            setErrors('Price is not a number')
+            return false;
+        }
+        return true;
+    };
+
+
     const onCreate =() =>{
+
+        if(!validateInput()){
+            return;
+        }
+
+        console.warn('Creating Product',  name);
+
         //save in the database
         resetFields();
     }
 
     return(
         <View style={styles.container} >
+            <Image source = {{uri: defaultPizzaImage}}  style = {styles.image} />
+            <Text style={styles.textButton}> Select Image</Text>
             <Text style ={styles.label}> Name</Text>
             <TextInput 
                 value = {name}
@@ -33,6 +63,7 @@ const CreateProductScreen =() => {
                 placeholder='9.99'  
                 style= {styles.input} 
                 keyboardType='numeric'/>
+            <Text style={{color: 'red'}}>{errors}</Text>
             <Button onPress={onCreate} text='Create'/>
         </View>
         
@@ -44,6 +75,17 @@ const styles = StyleSheet.create({
         flex:1, //to make sure the objects spans the entire screen
         justifyContent:'center',
         padding:10,
+    },
+    image:{
+        width: '50%',
+        aspectRatio:1,
+        alignSelf:'center',
+    },
+    textButton:{
+        alignSelf:'center',
+        fontWeight: 'bold',
+        color: Colors.light.tint,
+        marginVertical:10,
     },
     input:{
         backgroundColor:'white',
