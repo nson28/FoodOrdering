1337f86d13dc8160c9894b041551f90d3a6373d3
nson28 <nelsontejara@vsu.edu.ph>
2024-12-06 13:56:05 +0800

    added order list item assignment

---
+++ src/app/(user)/orders/index.tsx
@@ -0,0 +1,14 @@
+import { Text , FlatList} from 'react-native';
+import orders from '@/assets/data/orders';
+import OrderListItem from '@/src/components/OrderListItem';
+
+export default function OrdersScreen(){
+    return (
+        <FlatList
+            data= {orders}
+            renderItem={({item})=> <OrderListItem order={item}/>}
+         
+        
+        />
+    )
+}
