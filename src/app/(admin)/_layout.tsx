771ee68a05438fb1ea0453cdf6757e1941e3ee4b
nson28 <nelsontejara@vsu.edu.ph>
2024-12-06 15:09:23 +0800

    assignment 2 added

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -44,7 +44,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
           title: 'Orders',
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
